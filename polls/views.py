e12a14d44b100450b7929d5c2ce57630406bac8e
Steve Aldrich <steve@cowichegrowers.com>
2026-05-27 15:05:27 -0700

    changed DB connection

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,18 @@
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404
 from .models import Question
-from django.template import loader
-
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("index.html")
     context = {"latest_question_list": latest_question_list}
-    return HttpResponse(template.render(context, request))
+    return render(request, render(request, "index.html", context))
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request,"detail.html", {"question": question})
 
 
 def results(request, question_id):
